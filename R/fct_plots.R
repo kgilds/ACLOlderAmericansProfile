61c7e18942a03b0fe6b0454f7594793d623a41fc
kgilds <kevingilds@gmail.com>
2021-05-12 06:44:17 -0400

    fix: tidying up and working on annotations

--- R/fct_plots.R
+++ R/fct_plots.R
@@ -3,7 +3,7 @@
 #' @description 
 #' Plot population points of Americans over the age of 65 by year and projections
 #' to 2060. 
-#' @import ggplot2
+#' @import ggplot2 ggtext
 #' @return
 #' @export
 #'
@@ -13,13 +13,21 @@ persongs_65_plot <- function() {
     aes(x = Year, y = population) +
     geom_step(size = 0.5, colour = "#f9a21a"
     ) +
+    geom_vline(xintercept = 2019, col="purple", linetype= "dashed", size =1) +
     labs(
       title = "Number of Persons 65 & Over 1900-2060",
       subtitle = "Number in Millions",
       caption = "Source: U.S. Census Bureau,
  Population Estimates and Projections"
     ) +
-    hrbrthemes::theme_modern_rc()
+    
+    geom_text(aes(x = 2040, y = 90,
+                      label = "Projected increase \n after 2018"),
+                  angle = 25)+
+    urbnthemes::theme_urbn_print(base_size = 8.5, base_family = "Lato",
+                     base_line_size = 0.5, base_rect_size = 0.5)
+  
+  
 }
 
 
@@ -37,7 +45,7 @@ number_state_map <- function(){
   plot_usmap(data = state_profile, values = "Number of Persons 65 and Older", labels = FALSE) +
     scale_fill_continuous( low = "white", high = "purple", 
                            name = "Population", label = scales::label_number_si()) + 
-    theme(legend.position = "right") + 
+     theme(legend.position = "right") + 
     theme(panel.background = element_rect(colour = "black")) + 
     labs(title = "Population of People 65 and Over by State", caption = "Source: @kgilds") 
   
