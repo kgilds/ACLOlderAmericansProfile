312e2634e1a7b5103d1cbb548f9b2ce8681f37ff
kgilds <kevingilds@gmail.com>
2021-05-12 06:40:23 -0400

    feat: add waiter, css, footer

--- R/app_ui.R
+++ R/app_ui.R
@@ -2,22 +2,27 @@
 #' 
 #' @param request Internal parameter for `{shiny}`. 
 #'     DO NOT REMOVE.
-#' @import shiny
+#' @import shiny 
 #' @noRd
 app_ui <- function(request) {
   tagList(
     # Leave this function for adding external resources
-    golem_add_external_resources(),
+    golem_add_external_resources(
+      
+      #tags$link(rel ="stylesheet", type="text/css", href="inst/app/www/custom.css")
+    ),
     # List the first level UI elements here 
-   
+    waiter::use_waiter(),
+    waiter::waiter_on_busy(waiter::spin_heart(), color = "purple"),
+    
     
     bs4Dash::bs4DashPage(
       
       enable_preloader = TRUE,
       
       navbar = bs4Dash::bs4DashNavbar(
-        status = "purple",  
-        "Profile of Older Americans"
+        status = "purple"
+        
         
       ),#navbar
       sidebar_collapsed = TRUE,
@@ -32,38 +37,47 @@ app_ui <- function(request) {
         
         bs4Dash::bs4SidebarMenuItem(
           "About",
-          tabName = "about_ui_1"
+          tabName = "about_ui_1",
+          icon = "info"
         ),
         
         bs4Dash::bs4SidebarMenuItem(
-          "Home",
+          "Aging Population",
           tabName = "population_65_plus_ui_1",
-          icon = "sliders"
+          icon = "hourglass"
         ),
         bs4Dash::bs4SidebarMenuItem(
           "State Profile",
-          tabName = "states_ui_1"
+          tabName = "states_ui_1",
+          icon = "globe"
         )
+        
+        
     )
       ),
     body = bs4Dash::bs4DashBody(
       bs4Dash::bs4TabItems(
+        mod_about_ui("about_ui_1"),
         mod_population_65_plus_ui("population_65_plus_ui_1"),
-        mod_states_ui("states_ui_1"),
-        mod_about_ui("about_ui_1")
-      )),#body
-    footer =  bs4DashFooter(
-      copyrights = a(
-        href = "https://kgilds.rbind.io/",
-        target = '_blank', "Developed by Kevin Gilds"
-      ),
-      
-      right_text = Sys.Date(),
-      center_text = ""
-      
-      
-    )
+        mod_states_ui("states_ui_1")
+        
+      )
+      ),#body
     
+    footer =  bs4Dash::bs4DashFooter(
+      h5("Built with",
+         img(src = "https://rstudio.com/wp-content/uploads/2014/04/shiny.png", height = "30px"),
+         "by",
+         img(src = "https://rstudio.com/wp-content/uploads/2014/07/RStudio-Logo-Blue-Gray.png", height = "30px")
+      
+      
+    ),
+    
+    right_text = a(
+      href = "https://kgilds.rbind.io/",
+      target = '_blank', "Created by Kevin Gilds"
+    )
+    )
     ) #page
   )
 }
@@ -89,7 +103,8 @@ golem_add_external_resources <- function(){
       app_title = 'ACLOlderAmericansProfile'
     ),
     # Add here other external resources
-    tags$link(rel="stylesheet", type="text/css", href="inst/app/www/custom.css")
+    
+    tags$link(rel="stylesheet", type="text/css", href="inst/app/www/style.css") 
     # for example, you can add shinyalert::useShinyalert() 
   )
 }
